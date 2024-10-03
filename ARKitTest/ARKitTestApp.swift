4bdfcc695ab27d08be78a4941de82549237c0558
MatthewKerns <101942557+MatthewKerns@users.noreply.github.com>
2024-10-03 09:48:48 -0700

    Initial Commit

---
+++ ARKitTest/ARKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitTestApp.swift
+//  ARKitTest
+//
+//  Created by Matthew Kerns on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ARKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
